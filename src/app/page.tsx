011e177e5d4514a39c777ba75c85c45e063480a8
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2025-02-10 10:51:22 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ const Page: React.FC = () => {
     <div className='bg-[#121212]'>
       <Header />
       <div className='relative mt-[5vh] md:mt-[8vh] bg-[#121212]'>
-        <div className='mb-5'>
+        <div className='mb-5 bg-[#121212]'>
           {/* Centered Search Bar */}
           <div className='mb-4 flex justify-center'>
             <input
