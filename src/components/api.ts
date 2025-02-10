0e2e8f79687885371be6f09e3523e5240a0bcd44
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2025-02-10 12:22:04 +0600

    Update api.ts

--- src/components/api.ts
+++ src/components/api.ts
@@ -5,14 +5,15 @@ const Data = [
     title: 'Scam 2003 - The Telgi Story (2023) (Part 2)',
     category: 'Drama',
     size: '1.65 GB',
+    url: 'https://drive.google.com/file/d/1XSP3y5t3-MlSU9huuEGNUN4T95-SBB97/view',
     downloadLinks: [
-      'https://drive.google.com/file/d/1XSP3y5t3-MlSU9huuEGNUN4T95-SBB97/view',
-      'https://anotherlink.com/file2',
+      'https://downloadlink1.com',
+      'https://downloadlink2.com'
     ],
     watchLinks: [
       'https://watchlink1.com',
-      'https://watchlink2.com',
-    ],
+      'https://watchlink2.com'
+    ]
   },
   {
     photo: '/img/Scam 2003 - The Telgi Story (2023).jpg',
@@ -20,12 +21,15 @@ const Data = [
     title: 'Scam 2003 - The Telgi Story (2023) (Part 1)',
     category: 'Drama',
     size: '4.85 GB',
+    url: 'https://drive.google.com/file/d/1iuigNIcUahU1QKG-B2G1bmXoyUUnFjna/view',
     downloadLinks: [
-      'https://drive.google.com/file/d/1iuigNIcUahU1QKG-B2G1bmXoyUUnFjna/view',
+      'https://downloadlink3.com',
+      'https://downloadlink4.com'
     ],
     watchLinks: [
       'https://watchlink3.com',
-    ],
+      'https://watchlink4.com'
+    ]
   }
 ];
 
