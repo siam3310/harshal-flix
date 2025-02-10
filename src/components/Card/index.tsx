ccb041fb2cca782a63abd6a11e2224ceb5b7503e
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2025-02-10 11:27:44 +0600

    Update index.tsx

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -13,35 +13,24 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ photo, title, size, url, key, index }) => {
   return (
     <div
-      className='mx-auto flex w-0 animate-[card-grow_300ms_ease_0s_1_normal_forwards] flex-col overflow-hidden rounded-xl bg-red-100  opacity-0  !shadow-red-300 ring-2 ring-red-300 ring-offset-2 transition-all
-    hover:shadow-[0.9em_0.9em_0em] active:!shadow-red-400'
-      key={key}
-      style={{ animationDelay: `${Math.min(index * 200, 1500)}ms` }}>
+      className='mx-auto flex w-full flex-col overflow-hidden rounded-xl bg-[#121212] text-[#D1D5DB] border-2 border-[#00FE94] p-5 shadow-md'>
       <Image
         src={photo}
         alt={title + ' image'}
         loading='lazy'
-        className='h-72 w-full cursor-pointer select-none object-cover'
+        className='h-72 w-full object-cover'
         height={288}
         width={224}
       />
-      <div className='mx-3 mt-3 flex grow flex-col  '>
-        <h6 className='relative cursor-default text-2xl md:text-xl lg:text-lg '>
-          {title}
-        </h6>
-        <span className='text relative mb-3 mt-auto cursor-default lg:text-sm'>
-          Size : {size}
-        </span>
+      <div className='mt-3 flex flex-col'>
+        <h6 className='font-bold text-xl'>{title}</h6>
+        <span className='text-sm mb-3 mt-auto'>Size: {size}</span>
         <a
           href={url}
           target='_blank'
           rel='noreferrer'
-          className='group relative mx-auto my-5 w-fit cursor-pointer select-none px-6 py-3 text-center font-bold text-red-600 transition-all active:translate-x-1 active:translate-y-1 active:text-red-900'>
-          <span
-            className=' absolute inset-0 size-full -translate-x-2 translate-y-2 rounded-xl bg-red-300 transition duration-300  ease-out group-hover:translate-x-2 group-hover:translate-y-2 
-                 group-active:translate-x-0 group-active:translate-y-0'></span>
-          <span className='absolute inset-0 size-full rounded-xl border-[3px] border-red-800'></span>
-          <span className='relative '>Download</span>
+          className='mx-auto mt-5 w-fit px-6 py-3 text-center font-bold text-[#00FE94] border-2 border-[#00FE94] rounded-lg'>
+          Download
         </a>
       </div>
     </div>
