1c4225eab580260d6ce5685c11d94bd2aa7d762a
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2025-02-10 11:22:57 +0600

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className='mt-10 bg-gray-900 text-gray-100'>
+    <footer className='mt-10 bg-[#121212] text-[#D1D5DB]'>
       <div className='mx-auto flex w-full max-w-6xl flex-col justify-between gap-x-5 p-5 md:flex-row'>
         <div>
           &copy; {new Date().getFullYear()} HarshalFlix. All rights reserved. |
-          Made by Harshal Katakiya
+          Made by Siam
         </div>
       </div>
     </footer>
